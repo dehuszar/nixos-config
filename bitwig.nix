0d657ecef22c9b0ae29042609d270fe5d8745609
Samuel Allen <dehuszar@gmail.com>
2026-09-06 20:27:34 -0400

    Pulling the bitwig beta config out in favor of new stable release
    the overrides to construct the beta download element have been moved to
    its own flake in the nix-flakes folder.

--- bitwig.nix
+++ bitwig.nix
@@ -1,22 +1,12 @@
 # bitwig.nix
 { pkgs, ... }:
 let
-  bitwig-studio-beta = pkgs.bitwig-studio.overrideAttrs (
-    finalAttrs: prevAttrs: {
-      version = "6.1-beta-6";
-      src = pkgs.fetchurl {
-        name = "bitwig-studio-${finalAttrs.version}.deb";
-        url = "https://www.bitwig.com/dl/Bitwig%20Studio/6.1%20Beta%206/installer_linux/";
-        hash = "sha256-XiC6OO9TsZkENdLxOr3jGdhVdiszvZ0ziSDFMBaPZcA=";
-      };
-    }
-  );
   bottles-overridden = pkgs.bottles.override { removeWarningPopup = true; };
 in
 {
   home.packages = [
-    bitwig-studio-beta
     bottles-overridden
+    pkgs.bitwig-studio
     pkgs.yabridge
     pkgs.yabridgectl
   ];
